89f82924fdda116389e19500d658e4a6303a6bdc
Pawel Stolowski <stolowski@gmail.com>
2019-10-24 13:47:53 +0200

    Test.

--- cmd/snap-start-preseed/main_test.go
+++ cmd/snap-start-preseed/main_test.go
@@ -117,7 +117,7 @@ func (s *startPreseedSuite) TestChrootFailure(c *C) {
 	c.Check(main.Run(parser, []string{tmpDir}), ErrorMatches, fmt.Sprintf("cannot chroot into %s: FAIL: %s", tmpDir, tmpDir))
 }
 
-func (s *startPreseedSuite) TestStartPrekeHappy(c *C) {
+func (s *startPreseedSuite) TestStartPrebakeHappy(c *C) {
 	tmpDir := c.MkDir()
 	dirs.SetRootDir(tmpDir)
 	mockChrootDirs(c, tmpDir)
@@ -138,7 +138,7 @@ func (s *startPreseedSuite) TestStartPrekeHappy(c *C) {
 	restoreMountPath := main.MockMountPath(targetSnapdRoot)
 	defer restoreMountPath()
 
-	restoreSystemSnapFromSeeds := main.MockSystemSnapFromSeeds(func(string) (string, error) { return "core.snap", nil })
+	restoreSystemSnapFromSeeds := main.MockSystemSnapFromSeeds(func(string) (string, error) { return "/a/core.snap", nil })
 	defer restoreSystemSnapFromSeeds()
 
 	c.Assert(os.MkdirAll(filepath.Join(targetSnapdRoot, "usr/lib/snapd/"), 0755), IsNil)
@@ -150,6 +150,12 @@ func (s *startPreseedSuite) TestStartPrekeHappy(c *C) {
 
 	parser := testParser(c)
 	c.Check(main.Run(parser, []string{tmpDir}), IsNil)
+
+	c.Assert(mockMountCmd.Calls(), HasLen, 1)
+	// note, tmpDir, targetSnapdRoot are contactenated again cause we're not really chrooting in the test
+	// and mocking dirs.RootDir
+	c.Check(mockMountCmd.Calls()[0], DeepEquals, []string{"mount", "-t", "squashfs", "/a/core.snap", filepath.Join(tmpDir, targetSnapdRoot)})
+
 	c.Assert(mockTargetSnapd.Calls(), HasLen, 1)
 	c.Check(mockTargetSnapd.Calls()[0], DeepEquals, []string{"snapd"})
 }
